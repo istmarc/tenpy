568612a39f2484c4fcf8012cab213a2397ffee4e
Marc Nawman <mnawman@proton.me>
2026-09-16 04:55:27 +0200

    Random walk paths

--- ten/process.py
+++ ten/process.py
@@ -5,7 +5,7 @@ from ten import dtype, storage_format, storage_order, tensor, diagonal
 """
 Random walk with a uniform distribution by default
 """
-def random_walk(n, k = 0., prob = .5, inc = 1., data_type = dtype.float32, prob_type = dtype.float32):
+def random_walk(n : int, k = 0., prob = .5, inc = 1., data_type = dtype.float32, prob_type = dtype.float32) -> tensor:
     if data_type == dtype.float32:
         if prob_type == dtype.float32:
             s = tencore.random_walk_float_float(n, k, prob, inc)
@@ -20,6 +20,26 @@ def random_walk(n, k = 0., prob = .5, inc = 1., data_type = dtype.float32, prob_
             raise RuntimeError("Probabilty type not supported.")
     else:
         raise RuntimeError("Unsupported data type")
+
+"""
+Random walk paths with a uniform distribution by default
+"""
+def random_walk_paths(n : int, t : int, k = 0., prob = .5, inc = 1., data_type = dtype.float32, prob_type = dtype.float32) -> tensor:
+    if data_type == dtype.float32:
+        if prob_type == dtype.float32:
+            s = tencore.random_walk_paths_float_float(n, t, k, prob, inc)
+            return tensor(s.shape(), data_type, s.format(), s.storage_order(), s)
+        else:
+            raise RuntimeError("Probabilty type not supported.")
+    elif data_type == dtype.float64:
+        if prob_type == dtype.float64:
+            s = tencore.random_walk_paths_double_double(n, t, k, prob, inc)
+            return tensor(s.shape(), data_type, s.format(), s.storage_order(), s)
+        else:
+            raise RuntimeError("Probabilty type not supported.")
+    else:
+        raise RuntimeError("Unsupported data type")
+
 
 
 """
