e9084e2caaa295aa6e5503b04655bdef90467a25
Marc Nawman <mnawman@proton.me>
2026-09-16 04:19:06 +0200

    Brownian motion

--- ten/process.py
+++ ten/process.py
@@ -22,3 +22,40 @@ def random_walk(n, k = 0., prob = .5, inc = 1., data_type = dtype.float32, prob_
         raise RuntimeError("Unsupported data type")
 
 
+"""
+Brownian motion
+"""
+def brownian_motion(s : tensor, t : int, data_type = dtype.float32) -> tensor:
+    if data_type == dtype.float32:
+        w = tencore.brownian_motion_float(s.data(), t)
+        return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+    elif data_type == dtype.float64:
+        w = tencore.brownian_motion_double(s.data(), t)
+        return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+    else:
+        raise RuntimeError("Data type not supported.")
+
+"""
+Brownian motion paths
+"""
+def brownian_motion_paths(n : int, t : int, s : tensor = None, data_type = dtype.float32) -> tensor:
+    if s is None:
+        if data_type == dtype.float32:
+            w = tencore.brownian_motion_all_paths_float(n, t)
+            return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+        elif data_type == dtype.float64:
+            w = tencore.brownian_motion_all_paths_double(n, t)
+            return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+        else:
+            raise RuntimeError("Data type not supported.")
+    else:
+        assert s.rank() == 2
+        if data_type == dtype.float32:
+            w = tencore.brownian_motion_paths_float(s.data(), n, t)
+            return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+        elif data_type == dtype.float64:
+            w = tencore.brownian_motion_paths_float(s.data(), n, t)
+            return tensor(w.shape(), data_type, w.format(), w.storage_order(), w)
+        else:
+            raise RuntimeError("Data type not supported.")
+
