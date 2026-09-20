3d20338b9073bdb3295bae03cf81c6d2b2ace248
Marc Nawman <mnawman@proton.me>
2026-09-20 12:04:10 +0200

    Linear model and polynomial regression

--- ten/ml.py
+++ ten/ml.py
@@ -1,5 +1,6 @@
 import tencore
 
+from tencore import lm_method
 from ten import dtype, tensor
 
 import matplotlib.pyplot as plt
@@ -41,4 +42,63 @@ def hist(x: tensor, cv = False, standartize = False, cumulative = False, nbins =
     h = histogram(x.dtype(), cv, standartize, cumulative, nbins)
     h.fit(x)
     h.plot(color = color, fill = fill, **args)
+
+def _get_linear_model(data_type, method):
+    if data_type == dtype.float32:
+        return tencore.linear_model_float(method)
+    elif data_type == dtype.float64:
+        return tencore.linear_model_double(method)
+    else:
+        raise RuntimeError("Data type not supported.")
+
+"""
+Linear model
+"""
+class linear_model(object):
+    def __init__(self, method = tencore.lm_method.qr, data_type = dtype.float32):
+        self.data_type = data_type
+        self.model = _get_linear_model(data_type, method)
+
+    def fit(self, x, y, fitted = True):
+        assert isinstance(x, tensor)
+        assert isinstance(y, tensor)
+        self.model.fit(x.data(), y.data(), fitted)
+
+    def coef(self):
+        beta = self.model.coef()
+        return tensor(beta.shape(), self.data_type, beta.format(), beta.storage_order(), beta)
+
+    def fitted():
+        yhat = self.model.fitted()
+        return tensor(yhat.shape(), self.data_type, yhat.format(), yhat.storage_order(), yhat)
+
+def _get_polyreg(data_type, n):
+    if data_type == dtype.float32:
+        return tencore.polyreg_float(n)
+    elif data_type == dtype.float64:
+        return tencore.polyreg_double(n)
+    else:
+        raise RuntimeError("Data type not supported.")
+
+"""
+Polynomial regression
+"""
+class polyreg(object):
+    def __init__(self, n, data_type = dtype.float32):
+        self.data_type = data_type
+        self.n = n
+        self.model = _get_polyreg(data_type, n)
+
+    def fit(self, x, y, fitted = True):
+        assert isinstance(x, tensor)
+        assert isinstance(y, tensor)
+        self.model.fit(x.data(), y.data(), fitted)
+
+    def coef(self):
+        beta = self.model.coef()
+        return tensor(beta.shape(), self.data_type, beta.format(), beta.storage_order(), beta)
+
+    def fitted(self):
+        yhat = self.model.fitted()
+        return tensor(yhat.shape(), self.data_type, yhat.format(), yhat.storage_order(), yhat)
 
