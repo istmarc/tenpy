727c7011ab56a49fdb27761c3d7d66205a967d7d
Marc Nawman <mnawman@proton.me>
2026-09-25 21:36:42 +0200

    Update polyreg and linear model

--- ten/ml.py
+++ ten/ml.py
@@ -9,37 +9,39 @@ import matplotlib.pyplot as plt
 Histogram
 """
 class histogram(object):
-    def __init__(self, data_type = dtype.float32, cv = False, standartize = False, cumulative = False, nbins = 0):
-        self.data_type = data_type
+    def __init__(self, cv = False, standartize = False, cumulative = False, nbins = 0):
         options = tencore.histogram_options(cv, standartize, cumulative, nbins)
         self.h = None
+
+    def fit(self, data):
+        assert isinstance(data, tensor)
+        assert data.rank() == 1
+        data_type = data.dtype()
         if data_type == dtype.float32:
             self.h = tencore.histogram_float(options)
         elif data_type == dtype.float64:
             self.h = tencore.histogram_double(options)
         else:
             raise RuntimeError("Data type not supported.")
-
-    def fit(self, data):
-        assert isinstance(data, tensor)
-        assert data.rank() == 1
-        assert data.dtype() == self.data_type
         self.h.fit(data.data())
 
     def hist(self):
         hist, bins = self.h.hist()
         return tensor(hist.shape(), hist.data_type(), hist.format(), hist.storage_order(), hist), tensor(bins.shape(), bins.data_type(), bins.format(), bins.storage_order(), bins)
 
-    def plot(self, color = "black", fill = "black", **args):
+    def plot(self, color = "black", fill = "black", figsize = None, **args):
         hist, bins = self.hist()
-        fig, ax = plt.subplots()
+        if figsize is None:
+            fig, ax = plt.subplots()
+        else:
+            fig, ax = plt.subplots(figsize = figsize)
         ax.stairs(hist.to_numpy(), bins.to_numpy(), color = color, fill = fill, **args)
 
 """
 Plot a histogram
 """
 def hist(x: tensor, cv = False, standartize = False, cumulative = False, nbins = 0, color = "black", fill = "black", **args):
-    h = histogram(x.dtype(), cv, standartize, cumulative, nbins)
+    h = histogram(cv, standartize, cumulative, nbins)
     h.fit(x)
     h.plot(color = color, fill = fill, **args)
 
@@ -55,13 +57,15 @@ def _get_linear_model(data_type, method):
 Linear model
 """
 class linear_model(object):
-    def __init__(self, method = tencore.lm_method.qr, data_type = dtype.float32):
-        self.data_type = data_type
-        self.model = _get_linear_model(data_type, method)
+    def __init__(self):
+        self.model = None 
 
-    def fit(self, x, y, fitted = True):
+    def fit(self, x, y, method = lm_method.qr, fitted = True):
         assert isinstance(x, tensor)
         assert isinstance(y, tensor)
+        assert x.dtype() == y.dtype()
+        data_type = x.dtype()
+        self.model = _get_linear_model(data_type, method)
         self.model.fit(x.data(), y.data(), fitted)
 
     def coef(self):
@@ -84,21 +88,24 @@ def _get_polyreg(data_type, n):
 Polynomial regression
 """
 class polyreg(object):
-    def __init__(self, n, data_type = dtype.float32):
-        self.data_type = data_type
+    def __init__(self, n):
         self.n = n
-        self.model = _get_polyreg(data_type, n)
+        self.model = None
+
 
     def fit(self, x, y, fitted = True):
         assert isinstance(x, tensor)
         assert isinstance(y, tensor)
+        assert x.dtype() == y.dtype()
+        data_type = x.dtype()
+        self.model = _get_polyreg(data_type, self.n)
         self.model.fit(x.data(), y.data(), fitted)
 
     def coef(self):
         beta = self.model.coef()
-        return tensor(beta.shape(), self.data_type, beta.format(), beta.storage_order(), beta)
+        return tensor(beta.shape(), beta.data_type(), beta.format(), beta.storage_order(), beta)
 
     def fitted(self):
         yhat = self.model.fitted()
-        return tensor(yhat.shape(), self.data_type, yhat.format(), yhat.storage_order(), yhat)
+        return tensor(yhat.shape(), yhat.data_type(), yhat.format(), yhat.storage_order(), yhat)
 
